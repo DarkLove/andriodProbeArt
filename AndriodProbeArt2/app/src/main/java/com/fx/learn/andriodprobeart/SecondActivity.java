b3494df5bd2ea492225167ee2cebc6f0e9f31da3
DarkLove <15874908437@163.com>
2017-09-23 13:59:30 +0800

    1

--- AndriodProbeArt2/app/src/main/java/com/fx/learn/andriodprobeart/SecondActivity.java
+++ AndriodProbeArt2/app/src/main/java/com/fx/learn/andriodprobeart/SecondActivity.java
@@ -1,15 +1,41 @@
 package com.fx.learn.andriodprobeart;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SecondActivity extends AppCompatActivity {
-
+    EditText et ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        et = (EditText) findViewById(R.id.et);
+
         UI.print("onCreate Second");
+        UI.toast(this,"Second onCreate");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("et",et.getText().toString());
+        UI.print("onSaveInstanceState");
+        UI.toast(this,"onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String et_string = savedInstanceState.getString("et");
+        et.setText(et_string);
+        UI.print("onRestoreInstanceState");
+        UI.toast(this,"onRestoreInstanceState");
     }
 
     @Override
--- AndriodProbeArt2/app/src/main/java/com/fx/learn/andriodprobeart/UI.java
+++ AndriodProbeArt2/app/src/main/java/com/fx/learn/andriodprobeart/UI.java
@@ -1,13 +1,20 @@
 package com.fx.learn.andriodprobeart;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2017/9/21.
  */
 
 public class UI {
+
     public static void print(String str){
-        Log.d("TAGXF",str);
+        Log.d("ZZZZZZF",str);
+    }
+
+    public static void toast(Context context,String msg){
+        Toast.makeText(context,msg,Toast.LENGTH_SHORT).show();
     }
 }
